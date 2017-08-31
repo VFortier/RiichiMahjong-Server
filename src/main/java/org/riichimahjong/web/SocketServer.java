4c14329733babcceb6af7b4939983125bd3f476d
Vincent Fortier <vincentf26@gmail.com>
2017-08-31 15:25:45 -0400

    Move server port and hostname to application.properties

--- src/main/java/org/riichimahjong/web/SocketServer.java
+++ src/main/java/org/riichimahjong/web/SocketServer.java
@@ -6,13 +6,10 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.UUID;
 
-import org.riichimahjong.web.data.Game;
 import org.riichimahjong.web.data.Player;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.logging.LogFile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -28,47 +25,52 @@ import com.corundumstudio.socketio.listener.DataListener;
 @ComponentScan("org.riichimahjong")
 @PropertySource("application-dev.properties") // TODO: automate env
 public class SocketServer {
-	
-	@Value("${client.url}")
-	private final String clientUrl = "";
-	
+
+	@Value("${server.port}")
+	private int port = -1;
+
+	@Value("${server.hostname}")
+	private String hostname = "";
+
 	private Map<UUID, ClientPlayer> playersInQueue = new LinkedHashMap<>();
 
     @Bean(name="webSocketServer")
     public SocketIOServer webSocketServer() {
 
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-        config.setHostname("localhost"); // TODO
-        config.setPort(9092);  // TODO
-        // config.setOrigin("localhost:4200");	// TODO
+        config.setHostname(hostname);
+        config.setPort(port);
 
         final SocketIOServer server = new SocketIOServer(config);
-        
+
         server.addConnectListener(new ConnectListener() {
             @Override
             public void onConnect(SocketIOClient client) {
             	System.out.println("meow");
             }
         });
-        
+
         server.addEventListener("find_game", Player.class, new DataListener<Player>() {
             @Override
             public void onData(SocketIOClient client, Player player, AckRequest ackRequest) {
-            	
+
             	UUID sessionId = client.getSessionId();
-            	// TODO: Check if client is already in playersInQueue
+            	if (playersInQueue.get(playersInQueue) != null) {
+        			return;
+            	}
+
             	playersInQueue.put(sessionId, new ClientPlayer(client, player));
-	            	
+
 	       		if (playersInQueue.size() >= 4) {
 	       			Iterator<Entry<UUID, ClientPlayer>> playersIterator = playersInQueue.entrySet().iterator();
 	       			List<ClientPlayer> playersForGame = new ArrayList<>();
-	       			
+
 	       			for (int i = 0; i < 4; i++) {
 	       				Entry<UUID, ClientPlayer> playerEntry = playersIterator.next();
 	       			    playersForGame.add(playerEntry.getValue());
-	       			    // TODO: remove from map
+	       			    playersIterator.remove();
 	       			}
-	       			
+
 	       			for (ClientPlayer clientPlayer : playersForGame) {
 	       				clientPlayer.getClient().sendEvent("game_found", "{ \"meow\": \"woof\" }");
 	       			}
