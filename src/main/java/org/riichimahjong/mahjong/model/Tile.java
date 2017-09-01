5d44379fc6d044ef4ecb37a478372b288ee8b17e
jupouliot <ju.pouliot@gmail.com>
2017-09-01 12:45:12 -0400

    changed git ignore, added tilesets and tile object

---
+++ src/main/java/org/riichimahjong/mahjong/model/Tile.java
@@ -0,0 +1,8 @@
+package org.riichimahjong.mahjong.model;
+
+public class Tile {
+    private String _tileShortName;  //a string describing the tile
+    private String _tileLongName;   //the displayed name of a tile
+    private boolean _redTile = false;
+
+}
